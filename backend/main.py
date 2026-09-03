bed180f1cdb8144e77a2701b39617c3ac1d3f44c
apariciodevme <apariciodev@proton.me>
2026-09-03 14:38:12 +0200

    pre deployment config

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,9 @@ app = FastAPI()
 # Allow our Next.js frontend to communicate with this API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
